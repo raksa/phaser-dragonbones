0a4d312caca9c185f3ffe31c1c929747d6dd0d7f
raksa <eng.raksa@gmail.com>
2017-04-13 16:40:55 +0700

    improve playing animation

--- example/js/index.js
+++ example/js/index.js
@@ -1,44 +1,36 @@
 "use strict";
 
 var model = "Swordsman";
-var animations = [
-    'steady', 'steady2', 'attack1', 'attack1_+1', 'attack2', 'jump', 'walk', 'walk2',
-];
 var sprite;
+var index = -1;
+
 new Phaser.Game("100%", "100%", Phaser.CANVAS, '', {
     preload: function () {
-        this.PhaserBones = this.game.plugins.add(Rift.PhaserBones);
-        this.PhaserBones.AddResourceByName(model,
+        this.dragonBonesPlugin = this.game.plugins.add(Rift.DragonBonesPlugin);
+        this.dragonBonesPlugin.AddResourceByName(model,
             "Swordsman/SwordsMan_ske.json", "Swordsman/SwordsMan_tex.json", "Swordsman/SwordsMan_tex.png");
-        this.PhaserBones.LoadResources();
+        this.dragonBonesPlugin.LoadResources();
     },
     create: function () {
         this.add.text(this.world.width / 2, 30, "Tap/Click to change animation").anchor.setTo(0.5);
 
         this.stage.backgroundColor = "#fefefe";
 
-        var group = this.add.group();
-        group.game = this;
-        group.index = -1;
-        group.renewSprite = function () {
-            this.removeAll();
-            this.index = (this.index + 1) % animations.length;
-            this.add(this.game.createSprite(animations[this.index]));
-        };
-        group.renewSprite();
-
-        this.input.onDown.add(function (event) {
-            this.renewSprite();
-        }, group);
-    },
-    createSprite: function (animation) {
-        sprite = this.PhaserBones.GetArmature(model);
+        sprite = this.dragonBonesPlugin.GetArmature(model);
         sprite.position.setTo(this.world.width / 2, 3 * this.world.height / 4);
-        sprite.animate(animation);
         sprite.scale.setTo(0.6);
-        return sprite;
+        this.world.add(sprite);
+
+        this.changeAnimation();
+
+        this.input.onDown.add(this.changeAnimation, this);
+    },
+    changeAnimation: function (animation) {
+        var names = sprite.animation._animationNames;
+        index = (index + 1) % names.length;
+        sprite.animation.play(names[index]);
     },
     update: function () {
-        // this.PhaserBones.RefreshClock(); // when switching states this needs called
+        // this.dragonBonesPlugin.RefreshClock(); // when switching states this needs called
     }
 });
