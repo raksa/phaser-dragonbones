e1e14b7c9104fa3f013ba78c8b9d48051b667eb8
raksa <eng.raksa@gmail.com>
2017-04-18 12:10:29 +0700

    start 0.0.4

--- src/DragonBonesPlugin.ts
+++ src/DragonBonesPlugin.ts
@@ -1,7 +1,7 @@
 namespace Rift {
     export const IMAGE: number = 2;
     export const JSON: number = 11;
-    export const VERSION: string = "0.0.2";
+    export const VERSION: string = "0.0.4";
 
     export class DragonBonesPlugin extends Phaser.Plugin {
         private static objDictionary: {
